d13d342e7a34bd4433cf525004c49fe176fba210
Tom Weimer <93084975+TomWeimer@users.noreply.github.com>
2022-05-04 18:14:24 +0200

    bonus

--- src/bonus/ft_lstmap.c
+++ src/bonus/ft_lstmap.c
@@ -1,29 +1,42 @@
-#include "libft.h"
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-    t_list *new_list;
-    t_list *first;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tweimer <tweimer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 18:01:54 by tweimer           #+#    #+#             */
+/*   Updated: 2022/05/04 18:10:02 by tweimer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	if (!del||!f)
-		return(NULL);
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*first;
+
+	if (!del || !f)
+		return (NULL);
 	first = NULL;
-	while(lst)
+	while (lst)
 	{
-		if (!(new_list = ft_lstnew((*f)(lst->content))))
+		new_list = ft_lstnew((*f)(lst->content));
+		if (!new_list)
 		{
-			while(first)
+			while (first)
 			{
 				new_list = first->next;
 				(*del)(first->content);
 				free(first);
 				first = new_list;
 			}
-		lst = NULL;
-		return(NULL);
+			lst = NULL;
+			return (NULL);
 		}
 		ft_lstadd_back(&first, new_list);
 		lst = lst->next;
-   	}
-    return (first);
+	}
+	return (first);
 }
-
